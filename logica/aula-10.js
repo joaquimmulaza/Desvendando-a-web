714c5b2c43d540b771b08f17a133ca82b66d4945
Joaquim Mulaza <joaquimmulazadev@gmail.com>
2023-11-07 13:42:41 +0100

    feat: operadores em js

---
+++ logica/aula-10.js
@@ -0,0 +1,11 @@
+//OPERADORES EM JAVASCRIPT
+
+// + SOMA
+// * MULTIPLICAÇAO
+// / DIVISAO
+
+// % RESTO
+// ** ELEVADO
+
+console.log(4 ** 2) // 4 elevado a 2
+console.log(14 % 2) // resto da divisao de 14 por 2
