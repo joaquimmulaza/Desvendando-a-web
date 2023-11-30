940568cb461b4bde2e4a08eebd223c527e7302cb
Joaquim Mulaza <joaquimmulazadev@gmail.com>
2023-11-30 07:17:19 +0100

    feat: aula-11 more about strings

---
+++ logica/aula-11.js
@@ -0,0 +1,18 @@
+const mensagem = "Ola mundo"
+const mensagem2 = "Hello world"
+
+console.log(mensagem.split(' ')) // QUEBRA A STRING EM ARRAY
+
+console.log(mensagem.length) // VER O TAMANHO DA STRING (CARACTERES)
+
+console.log(mensagem.concat(mensagem2)) //CONCATENA UMA STRING COM OUTRA
+
+console.log(mensagem + mensagem2) //CONCATENA UMA STRING COM OUTRA
+
+console.log(`${mensagem} ${mensagem2}`) //CONCATENA UMA STRING COM OUTRA
+
+console.log(mensagem.toLowerCase()) //COLOCA TODAS PALAVRAS EM MINUSCULA
+
+console.log(mensagem.toUpperCase()) //COLOCA TODAS PALAVRAS EM MAIUSCULA
+
+console.log(mensagem.replace('mundo', 'pessoal')) //TROCA UMA STRING POR OUTRA
